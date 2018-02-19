30eb26ab1182320ab3d7134d43683f25a45b52a4
Junyoung Lee <junyoung.plum@gmail.com>
2018-02-20 02:02:22 +0900

    Pomodoro Timer 예제 따라하기

---
+++ ch1/pomodoro/pomodoro.js
@@ -0,0 +1,13 @@
+const POMODORO_STATES = {
+			WORK: 'work',
+			REST: 'rest'
+		};
+
+const STATES = {
+	STARTED: 'started',
+	STOPPED: 'stopped',
+	PAUSED: 'paused'
+};
+
+const WORKING_TIME_LENGTH_IN_MINUTES = 25;
+const RESTING_TIME_LENGTH_IN_MINUTES = 5;
